7fe5c977b89c644d4faaa6e46559c450e4f6343b
Sergey Parmezhov <parmezhvsergey@mail.ru>
2021-08-15 22:52:08 +0300

    1 раздел

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
 function truncate(str, maxlength) {
   // ваш код...
+  let lenght = str.length;
+  let max = maxlength - 1;
+  if (lenght > maxlength) {
+    let stroka = str.slice(0, max) + "…";
+    return stroka;
+  };
+  if (lenght <= maxlength) {
+    return str;
+  };
 }
